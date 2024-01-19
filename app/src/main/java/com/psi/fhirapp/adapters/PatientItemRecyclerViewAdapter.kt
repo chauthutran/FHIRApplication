d786d8516579d0081d5aaab830cfdb043b3c47d9
chauthutran <cthutran@gmail.com>
2024-01-19 20:04:50 +0900

    Updated

--- app/src/main/java/com/psi/fhirapp/adapters/PatientItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/psi/fhirapp/adapters/PatientItemRecyclerViewAdapter.kt
@@ -36,3 +36,21 @@ class PatientItemRecyclerViewAdapter (val onClickItem: (view: View, patientItem:
         }
     }
 }
+
+class PatientDataDiffCallback  : DiffUtil.ItemCallback<PatientItem>() {
+
+    override fun areItemsTheSame(oldItem: PatientItem, newItem: PatientItem) : Boolean {
+        // Patient properties may have changed if reloaded from the DB, but ID is fixed
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: PatientItem,
+        newItem: PatientItem,
+    ) : Boolean {
+        // NOTE: if you use equals, your object must properly override Object#equals()
+        // Incorrectly returning false here will result in too many animations.
+        return oldItem.id == newItem.id && oldItem.risk == newItem.risk
+    }
+
+}
